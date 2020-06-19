c45be8d2555b34341caff8949e5217962c972efa
Fernando Giovanini <fernando.giovanini@coliquio.de>
2020-06-19 19:04:16 +0200

    Added iframe

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -129,5 +129,13 @@ ClassicEditor.defaultConfig = {
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	language: 'en',
+	mediaEmbed: {
+		extraProviders: [
+			{
+				name: 'iframe',
+				url: /^/
+			}
+		]
+	}
 };
